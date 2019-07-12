2d971edfd0600863becbd46e522b5bd0c0d00b41
kishorebiyyapu <36230007+kishorebiyyapu@users.noreply.github.com>
2019-07-12 19:20:24 +0530

    Update prog17.c

--- prog17.c
+++ prog17.c
@@ -3,9 +3,15 @@ int main()
 {
 	int L,M,i=2;
 	scanf("%d%d",&L,&M);
+	if(L==1 && M==1)
+	    printf("1");
+	else
+	{
+		    
 	while(i%L!=0 || i%M!=0){
 		i++;
 	}
 	printf("%d",i);
+    }
 	return 0;
 }
