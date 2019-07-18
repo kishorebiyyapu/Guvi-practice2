e85f288b1aa77dd23d055998ec59a9fd3d5e19d1
kishorebiyyapu <36230007+kishorebiyyapu@users.noreply.github.com>
2019-07-18 19:03:15 +0530

    Update prog9.py

--- prog9.py
+++ prog9.py
@@ -1,8 +1,10 @@
 l,r=map(int,input().split())
+a=[]
 for i in range(l+1,r):
     c=0
     for j in range(2,i//2+1):
         if i%j==0:
             c=c+1
     if c==0:
-        print(i,end=" ")
+        a.append(i)
+print(len(a))
