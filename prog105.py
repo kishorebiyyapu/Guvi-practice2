7cdfd17b6cb5ee95bad9298bff1974d6d6887941
kishorebiyyapu <36230007+kishorebiyyapu@users.noreply.github.com>
2019-08-02 17:54:06 +0530

    Update prog105.py

--- prog105.py
+++ prog105.py
@@ -1,10 +1,10 @@
 N=int(input())
-a=[int(i) for i in input().split()[:N]]
+b=[int(i) for i in input().split()[:N]]
 d=dict()
-for i in a:
-    d[a.index(i)+1]=i
-a.sort()    
-for i in a:
+for i in b:
+    d[b.index(i)+1]=i
+b.sort()    
+for i in b:
     for key,value in d.items():
         if i==value:
             print(key,end=' ')
