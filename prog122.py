aff94986a18de267a34a8bbfca96936ddf4068ea
kishorebiyyapu <36230007+kishorebiyyapu@users.noreply.github.com>
2019-08-07 19:28:09 +0530

    Create prog122.py

---
+++ prog122.py
@@ -0,0 +1,25 @@
+a,b,c=input().split('-')
+if b=='01':
+    print("January")
+elif b=='02':
+    print("Febrary")
+elif b=='03':
+    print("March")
+elif b=='04':
+    print("April")
+elif b=='05':
+    print("May")
+elif b=='06':
+    print("June")
+elif b=='07':
+    print("July")
+elif b=='08':
+    print("August")
+elif b=='09':
+    print("September")
+elif b=='10':
+    print("October")
+elif b=='11':
+    print("November")
+elif b=='12':
+    print("December")
